20a3d3245d6049b13859aea0a0d833c63283db13
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2026-07-30 14:11:29 -0400

    update app requirments

--- apps/stickers-web/src/main.ts
+++ apps/stickers-web/src/main.ts
@@ -4,6 +4,7 @@ import "./style.css";
 console.log(embeddingUrl);
 
 // Use eigen-db (already in package.json, https://github.com/chuanqisun/eigen-db) to import the embedding binary file from the url
+// The db binary file is available in public/embeddings.bin
 // - ideally show import progress, if available
 // Add password input box (disable autofill or any password manage integration)
 // - allow user to enter gemini api key
@@ -16,5 +17,6 @@ console.log(embeddingUrl);
 // - Sort the resulting laptop images by the highest sticker match score within the image
 // - Draw visual overlay on the laptop images, highlighting the bounding box and match score of the matches in that image. Some laptop image may have more than 1 highlights
 // - The entire system should respond to user input live, as user types, adjust k, and similarity limit, the result should update in realtime
+// All the laptop images data and bounding box data are in public/images folder. You need to result the filenames at runtime for perf reasons
 // You may use rxjs to handle async behavior
 // You may use lit's render function and html template literals to handle templating and event binding
